f097d66e4d58757b11f4030f5cd444e83867259c
Luigi Gresele <luigi.gresele@gmail.com>
2021-02-18 11:18:58 +0100

    added comments on metric

---
+++ metrics.py
@@ -0,0 +1,37 @@
+from jax import numpy as np 
+    
+def mMI(Jf, s):
+    '''
+    Mutual Information inspired independence of mechanisms metric
+    
+    Input:
+    Jf: batched Jacobian (function)
+    Applied to s, which has shape (N,D), returns a Jacobian with shape (N,D,D)
+    
+    s: a collection of samples of (true or reconstructed) sources, which has shape (N,D)
+    
+    Output:
+    
+    metric
+    '''
+    
+    # Get shapes and dimensions
+    N = s.shape[0]
+    
+    # Compute the Jacobian
+    Jacf = Jf(s)
+
+    # Compute the norm of its columns
+    grad_norms = np.linalg.norm(Jacf, axis=1)
+    log_grad_norms = np.log(grad_norms)
+
+    # Sum the norms
+    sum_log_norms = np.sum(log_grad_norms, axis = 1)
+
+    # Compute the determinants of the Jacobians
+    # N.B. Jacf needs to be of shape (..., M, M)
+    
+    # Just use slogdet here!
+    jac_log_dets = np.linalg.slogdet(Jacf)[1]
+    
+    return np.sum(sum_log_norms - jac_log_dets)/ N
