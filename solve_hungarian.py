f22066f13ba2f81e1e4fdd0c5f04ece6bbc28dd3
Luigi Gresele <luigi.gresele@gmail.com>
2021-05-14 00:38:57 +0200

    added functionality for Spearman correlation

--- solve_hungarian.py
+++ solve_hungarian.py
@@ -1,12 +1,17 @@
 import numpy as np
+from scipy.stats import spearmanr
 from scipy.optimize import linear_sum_assignment
 
-def SolveHungarian( recov, source ):
-	"""
-	compute maximum correlations between true indep components and estimated components 
-	"""
-	Ncomp = source.shape[1]
-	CorMat = (np.abs(np.corrcoef( recov.T, source.T ) ) )[:Ncomp, Ncomp:]
-	ii = linear_sum_assignment( -1*CorMat )
+def SolveHungarian( recov, source, correlation='Pearson' ):
+    """
+    compute maximum correlations between true indep components and estimated components 
+    """
+    Ncomp = source.shape[1]
+    if correlation == 'Pearson':
+        CorMat = (np.abs(np.corrcoef( recov.T, source.T ) ) )[:Ncomp, Ncomp:]
+    elif correlation == 'Spearman':
+        rho, _ = np.abs(spearmanr( recov, source ) )
+        CorMat = rho[:Ncomp, Ncomp:]
+    ii = linear_sum_assignment( -1*CorMat )
 
-	return CorMat[ii].mean(), CorMat, ii
+    return CorMat[ii].mean(), CorMat, ii
