72323bbbae305c1a0013122c16bfd4b63656a472
Luigi Gresele <luigi.gresele@gmail.com>
2021-05-15 15:32:05 +0200

    got rid of the metric with brute-force on the permutation matrices for now

--- metrics.py
+++ metrics.py
@@ -161,83 +161,6 @@ def jacobian_amari_distance(x, jac_r_unmix, jac_t_mix, unmixing_batched):
     
     return jnp.mean(amari_distance(J_r_unmix, J_t_mix))
 
-################################################################################################################################################################################################
-##
-################################################################################################################################################################################################
-
-def amari_inspired_distance(W, A, perm):
-    """
-    Computes the Amari distance between the products of two collections of matrices W and A.
-    It cancels when the average of the absolute value of WA is a permutation and scale matrix.
-    
-    Based on the implementation of Amari distance in:
-    https://github.com/pierreablin/picard/blob/master/picard/_tools.py
-    
-    Parameters
-    ----------
-    W : ndarray, shape (n_samples, n_features, n_features)
-        Input collection of matrices
-    A : ndarray, shape (n_samples, n_features, n_features)
-        Input collection of matrices
-    perm : ndarray, shape (n_features, n_features)
-        A permutation matrix
-    Returns
-    -------
-    d : ndarray, shape (n_samples, )
-        The Amari inspired distance between the average of absolute values of the products of W and A (and P).
-        This measures whether the product of W, A and P is diagonal.
-    """
-    
-    P = jnp.matmul(W, A)
-    P = jnp.matmul(P, perm)
-#     P = jnp.mean(jnp.abs(jnp.matmul(W, A)), axis=0)
-    
-    p = jnp.diagonal(P, axis1=1, axis2=2) # Here only the diagonal elements
-#     print(p.shape)
-    def s(r, p):
-        return jnp.sum(jnp.sum(r ** 2, axis=-1) / p ** 2 - 1, axis=-1)
-    return (s(jnp.abs(P), p) + s(jnp.abs(jnp.transpose(P, (0, 2, 1))), p)) / (2 * P.shape[1])
-#     return (s(jnp.abs(P)) + s(jnp.abs(P.T))) / (2 * P.shape[1])
-
-def jacobian_amari_inspired_distance(x, jac_r_unmix, jac_t_mix, unmixing_batched):
-    """
-    Computes an Amari-inspired distance between the products of two collections of **batched** Jacobians evaluated at points x.
-    It cancels when the composition of the functions whose Jacobians we multiply is a scalar function times a permutation.
-    It requires a bruteforce permutation through all permutation matrices.
-    
-    Based on the implementation of Amari distance in:
-    https://github.com/pierreablin/picard/blob/master/picard/_tools.py
-    
-    Parameters
-    ----------
-    x : ndarray, shape (n_samples, n_features)
-        Input collection of datapoints
-    jac_r_unmix : jacobian function for the reconstructed unmixing (**batched**)
-    jac_t_mix : jacobian function for the true unmixing (**batched**)
-    unmixing_batched : true unmixing (**batched**)
-    
-    Returns
-    -------
-    d: scalar
-        Average of the Amari distance between the products of the two collections of Jacobians, evaluated in x and unmixing_batched(x) respectively.
-    """
-#     J_r_unmix = jax.vmap(jac_r_unmix)(x)
-#     J_t_mix = jax.vmap(jac_t_mix)(unmixing_batched(x))
-    J_r_unmix = jac_r_unmix(x)
-    J_t_mix = jac_t_mix(unmixing_batched(x))
-    
-    minimum = 10**20
-    
-    # find all of the permutation matrices
-    a = jnp.arange(x.shape[1])
-    for p in multiset_permutations(a):
-        perm = np.zeros((x.shape[1], x.shape[1]))
-        perm[a, p]=1.0
-        ad1 = jnp.mean(amari_inspired_distance(J_r_unmix, J_t_mix, perm))    
-        if ad1<minimum:
-            minimum = ad1
-    return minimum
-
 
 ################################################################################################################################################################################################
 ##
@@ -267,3 +190,83 @@ def observed_data_likelihood(x, jac_unmixing, base_log_pdf="Uniform"):
     else:
         log_pdf = base_log_pdf(x)
     return log_det_jac + log_pdf
+
+# ################################################################################################################################################################################################
+# ##
+# ################################################################################################################################################################################################
+
+
+# # Below the distance based on bruteforce search on the permutation matrices -- not using it for now as too much variability when bad mixing.
+
+# def amari_inspired_distance(W, A, perm):
+#     """
+#     Computes the Amari distance between the products of two collections of matrices W and A.
+#     It cancels when the average of the absolute value of WA is a permutation and scale matrix.
+    
+#     Based on the implementation of Amari distance in:
+#     https://github.com/pierreablin/picard/blob/master/picard/_tools.py
+    
+#     Parameters
+#     ----------
+#     W : ndarray, shape (n_samples, n_features, n_features)
+#         Input collection of matrices
+#     A : ndarray, shape (n_samples, n_features, n_features)
+#         Input collection of matrices
+#     perm : ndarray, shape (n_features, n_features)
+#         A permutation matrix
+#     Returns
+#     -------
+#     d : ndarray, shape (n_samples, )
+#         The Amari inspired distance between the average of absolute values of the products of W and A (and P).
+#         This measures whether the product of W, A and P is diagonal.
+#     """
+    
+#     P = jnp.matmul(W, A)
+#     P = jnp.matmul(P, perm)
+# #     P = jnp.mean(jnp.abs(jnp.matmul(W, A)), axis=0)
+    
+#     p = jnp.diagonal(P, axis1=1, axis2=2) # Here only the diagonal elements
+# #     print(p.shape)
+#     def s(r, p):
+#         return jnp.sum(jnp.sum(r ** 2, axis=-1) / p ** 2 - 1, axis=-1)
+#     return (s(jnp.abs(P), p) + s(jnp.abs(jnp.transpose(P, (0, 2, 1))), p)) / (2 * P.shape[1])
+# #     return (s(jnp.abs(P)) + s(jnp.abs(P.T))) / (2 * P.shape[1])
+
+# def jacobian_amari_inspired_distance(x, jac_r_unmix, jac_t_mix, unmixing_batched):
+#     """
+#     Computes an Amari-inspired distance between the products of two collections of **batched** Jacobians evaluated at points x.
+#     It cancels when the composition of the functions whose Jacobians we multiply is a scalar function times a permutation.
+#     It requires a bruteforce permutation through all permutation matrices.
+    
+#     Based on the implementation of Amari distance in:
+#     https://github.com/pierreablin/picard/blob/master/picard/_tools.py
+    
+#     Parameters
+#     ----------
+#     x : ndarray, shape (n_samples, n_features)
+#         Input collection of datapoints
+#     jac_r_unmix : jacobian function for the reconstructed unmixing (**batched**)
+#     jac_t_mix : jacobian function for the true unmixing (**batched**)
+#     unmixing_batched : true unmixing (**batched**)
+    
+#     Returns
+#     -------
+#     d: scalar
+#         Average of the Amari distance between the products of the two collections of Jacobians, evaluated in x and unmixing_batched(x) respectively.
+#     """
+# #     J_r_unmix = jax.vmap(jac_r_unmix)(x)
+# #     J_t_mix = jax.vmap(jac_t_mix)(unmixing_batched(x))
+#     J_r_unmix = jac_r_unmix(x)
+#     J_t_mix = jac_t_mix(unmixing_batched(x))
+    
+#     minimum = 10**20
+    
+#     # find all of the permutation matrices
+#     a = jnp.arange(x.shape[1])
+#     for p in multiset_permutations(a):
+#         perm = np.zeros((x.shape[1], x.shape[1]))
+#         perm[a, p]=1.0
+#         ad1 = jnp.mean(amari_inspired_distance(J_r_unmix, J_t_mix, perm))    
+#         if ad1<minimum:
+#             minimum = ad1
+#     return minimum
