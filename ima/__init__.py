2a53407e0868ee3fd27242a21e749ef392a94f89
Vincent Stimper <vincent.stimper@gmail.com>
2021-06-04 14:17:31 +0200

    Imports of submodules added

---
+++ ima/__init__.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import metrics
+from . import mixing_functions
+from . import plotting
+from . import real_nvp
+from . import residual
+from . import solve_hungarian
+from . import utils
+
+__version__ = '0.1'
