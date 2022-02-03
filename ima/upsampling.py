9b5da334659819e0759d7c5513b77ee2ebcf3a0c
Vincent Stimper <vincent.stimper@gmail.com>
2022-02-03 15:07:07 +0100

    Upsampling added

--- ima/upsampling.py
+++ ima/upsampling.py
@@ -8,16 +8,17 @@ class Pad(distrax.Bijector):
     def __init__(self, pad):
         if isinstance(pad[0], int):
             pad = (pad,)
-        super.__init__(len(pad))
+        super().__init__(len(pad))
         self.pad = pad
 
-    def forward_log_det_jacobian(self, x):
-        y = jnp.pad(x, self.pad)
+    def forward_and_log_det(self, x):
+        pad = (len(x.shape) - len(self.pad)) * ((0, 0),) + self.pad
+        y = jnp.pad(x, pad)
         return y, jnp.zeros(x.shape[0])
 
-    def inverse_log_det_jacobian(self, y):
-        slices = [slice(0, None)]
+    def inverse_and_log_det(self, y):
+        slices = (len(y.shape) - len(self.pad)) * [slice(0, None)]
         for p in self.pad:
             e = None if p[1] == 0 else -p[1]
             slices.append(slice(p[0], e))
-        return y[type(slices)], jnp.zeros(y.shape[0])
+        return y[tuple(slices)], jnp.zeros(y.shape[0])
